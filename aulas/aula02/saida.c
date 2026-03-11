5cc36a26ba85d55750a84279e01400df40f23c06
unknown <2612130053@ACADEMICO.IESB.BR>
2026-03-11 10:57:33 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    //1. Entrada
+
+    //.2 Processamento
+
+    //3. Saida
+    printf("um texto legal\n");
+    printf("programar em c eh Top\n");
+    printf("10+10\n");
+        
+        return 0;
+}
